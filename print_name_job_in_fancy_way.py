9b36696d09b24eb510856fad91fbe3bf7a3ccb85
unknown <mangulabnanshervinmarco@gmail.com>
2024-03-24 16:35:01 +0700

    add blue collor on the goodluck message

--- print_name_job_in_fancy_way.py
+++ print_name_job_in_fancy_way.py
@@ -9,32 +9,29 @@ init(autoreset=True)
 
 # Define functions for printing text in different colors: "print_green", "print_yellow", and "print_orange".
 def print_green(text):
-    print(Fore.Green + text)
+    print(Fore.GREEN + text)
+
 def print_yellow(text):
-    print(Fore.Yellow + text)
-def print_orangen(text):
+    print(Fore.YELLOW + text)
+
+def print_orange(text):
     print(Fore.LIGHTRED_EX + text)
 
 # Prompt the user to enter their name, dream job, and dream city.
 
-print_green = ("Please enter your name: ")
-name = input ()
+print_green("Please enter your name: ")
+name = input()
 
-print_yellow = ("Please enter your dream job: ")
+print_yellow("Please enter your dream job: ")
 dream_job = input()
 
-print_orange = ("Please enter your dream city to work in: ")
+print_orange("Please enter your dream city to work in: ")
 dream_city = input()
 
 # Use "pyfiglet" to format the entered name, dream job, and dream city.
-formatted_name = pyfiglet.figlet_format(name)
-formatted_dream_job = pyfiglet.figlet_format(dream_job)
-formatted_dream_city = pyfiglet.figlet_format(dream_city)
+print_green(pyfiglet.figlet_format(name))
+print_yellow(pyfiglet.figlet_format(dream_job))
+print_orange(pyfiglet.figlet_format(dream_city))
 
-# Print the formatted text.
-print_name(formatted_name)
-print_dream_job(formatted_dream_job)
-print_dream_city(formatted_dream_city)
-
-# Print a message wishing the user good luck with their dream.
-print(f"Good luck, {name}! You can do it! You will be a/an {dream_job} in {dream_city} in the future!")
+# Print a message wishing the user good luck with their dream in blue color
+print("\033[94m" + f"Good luck, {name}! You can do it! You will be a/an {dream_job} in {dream_city} in the future!")
