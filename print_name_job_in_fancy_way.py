d60ef14186854c7801b9315defe0efc2b0e01065
unknown <mangulabnanshervinmarco@gmail.com>
2024-03-24 15:15:38 +0700

    added pseudocode

--- print_name_job_in_fancy_way.py
+++ print_name_job_in_fancy_way.py
@@ -1,10 +1,10 @@
 # Purpose: ask the user to input their name, dream job, and dream city to work in. Print them in fancy way.
 
-# 1. Define functions for printing text in different colors: "print_green", "print_yellow", and "print_orange".
-# 2. Prompt the user to enter their name, dream job, and dream city.
-# 3. Import necessary libraries: "pyfiglet" and "colorama".
-# 4. Initialize "colorama" for colored text.
-# 5. Print the prompts in the desired colors using the defined functions.
-# 6. Use "pyfiglet" to format the entered name, dream job, and dream city.
-# 7. Print the formatted text.
-# 8. Print a message wishing the user good luck with their dream.
+# Define functions for printing text in different colors: "print_green", "print_yellow", and "print_orange".
+# Prompt the user to enter their name, dream job, and dream city.
+# Import necessary libraries: "pyfiglet" and "colorama".
+# Initialize "colorama" for colored text.
+# Print the prompts in the desired colors using the defined functions.
+# Use "pyfiglet" to format the entered name, dream job, and dream city.
+# Print the formatted text.
+# Print a message wishing the user good luck with their dream.
